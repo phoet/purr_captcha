c9014bb43d98c6be075602da0535a47eabd4af6b
phoet <phoetmail@googlemail.com>
2011-07-27 17:02:46 +0200

    mounted the engine to the app

--- lib/purr_captcha.rb
+++ lib/purr_captcha.rb
@@ -1,14 +1,16 @@
 require 'httpi'
 require 'json'
 
-URL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=lolcat&tagmode=any&format=json&jsoncallback=_"
+require 'purr_captcha/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
 
-resp = HTTPI.get URL
-data = JSON.parse resp.body[2..-2]
-p data
-items = data['items']
-p items
-item = items[rand(items.size)]
-p item
-p item['title']
-p item['media']['m']
+# URL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=lolcat&tagmode=any&format=json&jsoncallback=_"
+# 
+# resp = HTTPI.get URL
+# data = JSON.parse resp.body[2..-2]
+# p data
+# items = data['items']
+# p items
+# item = items[rand(items.size)]
+# p item
+# p item['title']
+# p item['media']['m']
