110dacf24c1ae3e65428708941824f47ecc9f655
phoet <phoetmail@googlemail.com>
2011-07-28 13:21:07 +0200

    remove test-project

---
+++ spec/example_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'example' do
+
+  # let(:test_data) { 'testo' }
+
+  # before do
+  #   some_setup_stuff
+  # end
+
+  # context 'doing something' do
+  #   it 'should behave like i want to' do
+  #     it.should be_true
+  #   end
+  # end
+  
+  pending 'please remove this example and add your own spec!'
+
+end
--- testo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Testo::Application.config.secret_token = '2e4d46aeafb41f89cf6ace1e7d44cf25aee52b1f203b19f0c51179862e46a3b195ed8613ecf368b8abbe910c2e6aef69caf90d0e141b8750601ac32779ee9bdf'
